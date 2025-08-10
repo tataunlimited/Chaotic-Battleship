3e81d7f98fdad66dea4f54080729067f6c2ff787
Jason Mahar <jasonmahar@yahoo.com>
2025-08-10 19:21:04 -0400

    created EnemyWaveManger
    EnemyWaveManger can create a wave of enemy ships and set them in valid locations  on enemy board.
    
    In BoardController uncomment this line (35) to see all the enemy boats:
    enemyView.revealShips = true;

---
+++ Assets/_Scripts/Core/Ship/EnemyWaveManager.cs
@@ -0,0 +1,120 @@
+
+using Core.GridSystem;
+using Core.Board;
+
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using Random = System.Random;
+using UnityEngine.UIElements;
+using System.Drawing;
+using Unity.VisualScripting;
+
+
+namespace Core.Ship
+{
+    [System.Serializable]
+    public class EnemyWaveManager
+    {
+        public const int DEFAULT_NUM_SHIPS = 4;
+        public int[] DEFAULT_SHIP_LENGTHS = { 4, 3, 2, 1 };
+
+        public int waveNumber = 0;
+
+        private Random rnd = new Random();
+
+
+        // instantiate this as a singleton
+        private static readonly EnemyWaveManager instance = new EnemyWaveManager();
+
+        static EnemyWaveManager()
+        {
+        }
+
+        private EnemyWaveManager()
+        {
+        }
+
+        public static EnemyWaveManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public List<ShipModel> createDefaultWaveOfShips()
+        {
+            List<ShipModel> ships = new List<ShipModel>();
+
+            for (int i=0; i < DEFAULT_NUM_SHIPS; i++)
+            {
+                ships.Add(new ShipModel
+                {
+                    length = DEFAULT_SHIP_LENGTHS[i]
+                });
+            }
+
+            return ships;
+        }
+
+
+        // Takes in a wave of ships, randomly sets their rotation, and randomly sets  
+        // 		the x & y coordinates to valid positions of the BoardView's grid 
+        //
+        // The algorithm used is to test all possible locations on the board that the ship can fit
+        //      and randomly select one
+        //
+        // Returns: true - all ships are in valid locations
+        //          false - at least one of the ships could not be placed in a valid location
+        //
+        public bool randomlySetShipsLocations(BoardView board, List<ShipModel> ships)
+        {
+            List<GridPos> validLocations = new List<GridPos>();     // valid GridPos cells that the ship can fit
+            bool haveBeenSuccessfullyPlaced = true;
+
+            foreach (ShipModel ship in ships)
+            {
+                validLocations.Clear();
+
+                ship.orientation = (Orientation)rnd.Next(2);
+
+                int lastCol = board.width;
+                int lastRow = board.height;
+                // if the ship is horizontal, we know a ship of size > 1 won't fit in the rightmost columns, so don't bother testing those
+                if (ship.orientation == Orientation.Horizontal)
+                    lastCol = lastCol - ship.length + 1;
+
+                // if the ship is vertical, we know a ship of size > 1 won't fit in the bottommost rows, so don't bother testing those
+                else   
+                    lastRow = lastRow - ship.length + 1;
+
+                // test every position on the board to see if it is a valid GridPos, and if so, add to validLocations
+                for (int col = 0; col < lastCol; col++) 
+                {
+                    for (int row = 0; row < lastRow; row++)
+                    {
+                        GridPos root = new GridPos(col, row);
+                        if (board.Model.ValidateShipPlacement(root, ship.length, ship.orientation))
+                        {
+                            validLocations.Add(root);
+                        }
+                    }
+                }
+
+                if (validLocations.Count == 0)
+                    haveBeenSuccessfullyPlaced = false;
+                else 
+                { 
+                    // randomly choose one of the validLocations to set as the ship's root 
+                    int index = rnd.Next(validLocations.Count);
+                    ship.root = validLocations[index];
+                    board.TryPlaceShip(ship);
+                }
+            }
+
+            return haveBeenSuccessfullyPlaced;
+        }
+
+    }
+}
