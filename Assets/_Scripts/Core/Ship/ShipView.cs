45035ed5dfbe6ea478a9941f786bb2a5abee962b
Prashanna Kandel <40130947+PrashKandel63@users.noreply.github.com>
2025-09-13 22:36:25 -0400

    Update ShipView.cs

--- Assets/_Scripts/Core/Ship/ShipView.cs
+++ Assets/_Scripts/Core/Ship/ShipView.cs
@@ -277,7 +277,7 @@ namespace Core.Ship
 
             return isSunk;
         }
-
+        // TODO Add this into the Game Scence
         //private void FireTorpedo()
         //{
         //    if (torpedoPrefab != null && torpedoSpawnPoint != null)
