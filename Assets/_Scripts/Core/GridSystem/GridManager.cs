9ab31edf3255d4eb8895df4ef026e0a9a1be66f9
Prashanna Kandel <40130947+PrashKandel63@users.noreply.github.com>
2025-09-13 20:20:41 -0400

    Update GridManager.cs

--- Assets/_Scripts/Core/GridSystem/GridManager.cs
+++ Assets/_Scripts/Core/GridSystem/GridManager.cs
@@ -1,50 +1,47 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Core.GridSystem;
 
-namespace Core.GridSystem
+public class GridManager : MonoBehaviour
 {
-    public class GridManager : MonoBehaviour
+    public static GridManager Instance { get; private set; }
+    // The generic type `CellType`.
+    private Grid2D<CellType> grid;
+
+    private void Awake()
     {
-        public static GridManager Instance { get; private set; }
-
-        // The generic type `CellType`.
-        private Grid2D<CellType> grid;
-
-        private void Awake()
+        if (Instance != null && Instance != this)
         {
-            if (Instance != null && Instance != this)
-            {
-                Destroy(this);
-            }
-            else
-            {
-                Instance = this;
-            }
-
-            grid = new Grid2D<CellType>(10, 10, 1.0f, Vector3.zero, CellType.Empty);
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
         }
 
-        public bool InBounds(GridPos p)
-        {
-            return grid.InBounds(p);
-        }
+        grid = new Grid2D<CellType>(10, 10, 1.0f, Vector3.zero, CellType.Empty);
+    }
 
-        public CellType GetCellType(GridPos p)
-        {
-            return grid.Get(p);
-        }
+    public bool InBounds(GridPos p)
+    {
+        return grid.InBounds(p);
+    }
 
-        public bool WorldToGrid(Vector3 worldPosition, out GridPos gridPos)
-        {
-            return grid.WorldToGrid(worldPosition, out gridPos);
-        }
+    public CellType GetCellType(GridPos p)
+    {
+        return grid.Get(p);
+    }
 
-        public void PlaceShip(GridPos startPos)
+    public bool WorldToGrid(Vector3 worldPosition, out GridPos gridPos)
+    {
+        return grid.WorldToGrid(worldPosition, out gridPos);
+    }
+
+    public void PlaceShip(GridPos startPos)
+    {
+        if (grid.InBounds(startPos))
         {
-            if (grid.InBounds(startPos))
-            {
-                grid.Set(startPos, CellType.Ship);
-            }
+            grid.Set(startPos, CellType.Ship);
         }
     }
 }
